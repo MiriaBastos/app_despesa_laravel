b98bcb8d42e14aab53ee1c83d1dffae33a81bf2f
Miriã Bastos <142639375+MiriaBastos@users.noreply.github.com>
2024-03-18 00:10:50 -0300

    Adiciona links para o responsivo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -48,7 +48,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('Perfil') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
@@ -58,7 +58,7 @@
                             <x-dropdown-link :href="route('logout')"
                                 onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Sair') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -84,8 +84,14 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('dashboard')">
+                {{ __('Início') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="action('\App\Http\Controllers\DespesaController@getForm')">
+                {{ __('Cadastrar Despesas') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="action('\App\Http\Controllers\DespesaController@getLista')">
+                {{ __('Ver Despesas') }}
             </x-responsive-nav-link>
         </div>
 
