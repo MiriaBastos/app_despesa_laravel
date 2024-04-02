243c83d96aee7dd1568542fe61061cdbf009bc5a
Miriã Bastos <142639375+MiriaBastos@users.noreply.github.com>
2024-04-01 23:25:16 -0300

    Adiciona enum ao projeto

---
+++ app/Enums/tipoDespesas.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+use BenSampo\Enum\Attributes\Description;
+
+final class tipoDespesas extends Enum
+{
+    #[Description('Alimentação')]
+    const ALIMENTACAO = '1';
+
+    #[Description('Educação')]
+    const EDUCACAO = '2';
+
+    #[Description('Lazer')]
+    const LAZER = '3';
+
+    #[Description('Saúde')]
+    const SAUDE = '4';
+
+    #[Description('Transporte')]
+    const TRANSPORTE = '5';
+
+    #[Description('Empréstimo')]
+    const EMPRESTIMO = '6';
+
+    #[Description('Extras')]
+    const EXTRAS = '7';
+}
