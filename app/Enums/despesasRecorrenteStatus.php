5a2086bb6089003243aa0cfd00c73b2757783e52
Miriã Bastos <142639375+MiriaBastos@users.noreply.github.com>
2024-04-08 19:19:18 -0300

    Cria metodo no enum
    Cria metodo para formatar os labels

--- app/Enums/despesasRecorrenteStatus.php
+++ app/Enums/despesasRecorrenteStatus.php
@@ -7,9 +7,25 @@ use BenSampo\Enum\Attributes\Description;
 
 final class despesasRecorrenteStatus extends Enum
 {
-    #[Description('Inativo')]
-    const INATIVO = 0;
+    #[Description('Não Recorrente')]
+    const NAO_RECORRENTE = 0;
 
-    #[Description('Ativo')]
-    const ATIVO = 1;
+    #[Description('Recorrente')]
+    const RECORRENTE = 1;
+
+    public static function getHtmlLabel($value): string
+    {
+        $statusClass = 'default';
+
+        switch ($value) {
+            case self::RECORRENTE:
+                $statusClass = 'success';
+                break;
+            case self::NAO_RECORRENTE:
+                $statusClass = 'warning';
+                break;
+        }
+
+        return "<label class='label label-$statusClass'>" . self::getDescription($value) . "</label>";
+    }
 }
